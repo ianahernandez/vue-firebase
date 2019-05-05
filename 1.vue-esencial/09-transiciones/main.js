c5cc9a59bb98fbd638116a56257a274469742f59
Ana <anaceciliah95@gmail.com>
2019-05-04 23:04:50 -0400

    transition

--- 1.vue-esencial/09-transiciones/main.js
+++ 1.vue-esencial/09-transiciones/main.js
@@ -5,6 +5,12 @@
 const vm = new Vue({
     el: 'main',
     data: {
-
+        mostrar: true,
+        mensajes: {
+            transicion: 'Transiciones CSS con Vue.js',
+            animacion: 'Animaciones CSS con Vue.js',
+            animationCustom: 'Animaciones custom CSS con Vue.js',
+            entreElementos: 'Transicinoes entre elementos con Vue.js'
+        }
     }
 });
